330c374af70c3d436b53702f39e4016f50d4cbfe
Tomek Marciniak <tmarciniakm@gmail.com>
2024-03-23 19:44:47 +0100

    chore(repo): add base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/mina-connector/",
   plugins: [
     svelte({
       onwarn: (warning, handler) => {
